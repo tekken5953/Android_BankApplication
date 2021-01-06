a5b2383bcec1a51162a7384cf469cf9179ca0c22
tekken5953@naver.com <wodud1011>
2021-01-07 02:50:48 +0900

    WIP

--- BankApplication/app/src/main/java/app/bankapplication/HomePage.java
+++ BankApplication/app/src/main/java/app/bankapplication/HomePage.java
@@ -7,11 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
     TextView trading;
+    ImageView main_notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,16 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.home_page);
 
         trading = findViewById(R.id.main_account_gotrade);
+        main_notification = findViewById(R.id.main_notification);
+
+        main_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, CustomButton.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         trading.setOnClickListener(new View.OnClickListener() {
             @Override
